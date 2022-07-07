ef0b15b6b301ea137dd8dc02a13b2ecc6aac4927
albertolalanda <lalanda.alberto@gmail.com>
2022-07-07 13:03:35 +0100

    commit of finished tool

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
-# Random Image Generator
+# Random Number Image Generator
 
-A crude tool for generating random NFT images and metadata.
-
-See it in action in the [Web3 NFT Tutorial](https://youtu.be/meTpMP0J5E8) on YouTube.
+A crude tool for generating random number images and metadata for the purpose of creating NFTs.
+Can generate an image with a number from 0 to 9999 with the current settings.
+The number images can have 5 different background colors.
+10.000 num \* 5 bg = 50.000 combinations
+4 digit number ~ 0,9 probability
+3 digit number ~ 0,1 probability
+2 digit number ~ 0,01 probability
+1 digit number 0,001 probability
 
 ## Usage
 
 ```
+Change on index.js
+idx = number of images to generate
+max = max number to generate
+
+```
+
+```
+
 git clone <this-repo>
 npm install
 npm run randos
+
 ```
--- index.js
+++ index.js
@@ -1,123 +1,170 @@
-const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs');
-const sharp = require('sharp');
+const {
+  readFileSync,
+  writeFileSync,
+  readdirSync,
+  rmSync,
+  existsSync,
+  mkdirSync,
+} = require("fs");
+const sharp = require("sharp");
 
 const template = `
-    <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <!-- bg -->
-        <!-- head -->
-        <!-- hair -->
-        <!-- eyes -->
-        <!-- nose -->
-        <!-- mouth -->
-        <!-- beard -->
-    </svg>
-`
+    <svg width="<!-- width -->" height="114" viewBox="0 0 <!-- width --> 114" fill="none" xmlns="http://www.w3.org/2000/svg">
+    
+        <!-- bg1 -->
+        <!-- alg1 -->
 
-const takenNames = {};
-const takenFaces = {};
-let idx = 999;
+        <g transform="translate(83)">
+        <!-- bg2 -->
+        <!-- alg2 -->
+        </g>
+
+        <g transform="translate(166)">
+        <!-- bg3 -->
+        <!-- alg3 -->
+        </g>
+
+        <g transform="translate(249)">
+        <!-- bg4 -->
+        <!-- alg4 -->
+        </g>
+    </svg>
+`;
+
+const takenNumbers = {};
+// number of images to generate
+let idx = 20;
+// max number to generate e.g. 10000 (from 0 to 9999)
+const max = 10000;
+let widthOfImage = 83;
+
+let digitNames = new Map([
+  [0, "zero"],
+  [1, "one"],
+  [2, "two"],
+  [3, "three"],
+  [4, "four"],
+  [5, "five"],
+  [6, "six"],
+  [7, "seven"],
+  [8, "eight"],
+  [9, "nine"],
+]);
+let lengthProb = new Map([
+  [1, 0.001],
+  [2, 0.01],
+  [3, 0.1],
+  [4, 0.9],
+]);
 
 function randInt(max) {
-    return Math.floor(Math.random() * (max + 1));
+  return Math.floor(Math.random() * (max + 1));
 }
 
-function randElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+function getDigitAtPosition(number, position) {
+  return digitNames.get(parseInt(String(number)[position]));
 }
 
+function getName(number, numberLength) {
+  let result = "";
+  for (var i = 0; i < numberLength; i++) {
+    result = result + "-" + getDigitAtPosition(number, i);
+  }
 
-function getRandomName() {
-    const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
-    const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
-    
-    const randAdj = randElement(adjectives);
-    const randName = randElement(names);
-    const name =  `${randAdj}-${randName}`;
+  let name = result.substring(1);
 
-
-    if (takenNames[name] || !name) {
-        return getRandomName();
-    } else {
-        takenNames[name] = name;
-        return name;
-    }
+  return name;
 }
 
-function getLayer(name, skip=0.0) {
-    const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
-    const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
-    const layer = svg.match(re)[0];
-    return Math.random() > skip ? layer : '';
+function getLayer(name, skip = 0.0) {
+  const svg = readFileSync(`./layers/${name}.svg`, "utf-8");
+  const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g;
+  const layer = svg.match(re)[0];
+  return Math.random() > skip ? layer : "";
 }
 
 async function svgToPng(name) {
-    const src = `./out/${name}.svg`;
-    const dest = `./out/${name}.png`;
+  const src = `./out/${name}.svg`;
+  const dest = `./out/${name}.png`;
 
-    const img = await sharp(src);
-    const resized = await img.resize(1024);
-    await resized.toFile(dest);
+  const img = await sharp(src);
+  const resized = await img.resize(1024);
+  await resized.toFile(dest);
 }
-
 
 function createImage(idx) {
+  const bg = randInt(5);
+  const number = randInt(max - 1);
 
-    const bg = randInt(5);
-    const hair = randInt(7);
-    const eyes = randInt(9);
-    const nose = randInt(4); 
-    const mouth = randInt(5);
-    const beard = randInt(3);
-    // 18,900 combinations
+  // 10.000 * 5 bg = 50.000 combinations
+  // 4 digit ~ 0,9 probability
+  // 3 digit ~ 0,1 probability
+  // 2 digit ~ 0,01 probability
+  // 1 digit 0,001 probability
 
-    const face = [hair, eyes, mouth, nose, beard].join('');
+  const numberLength = number.toString().length;
 
-    if (face[takenFaces]) {
-        createImage();
-    } else {
-        const name = getRandomName()
-        console.log(name)
-        face[takenFaces] = face;
+  const combination = [bg, number].join("");
 
-        const final = template
-            .replace('<!-- bg -->', getLayer(`bg${bg}`))
-            .replace('<!-- head -->', getLayer('head0'))
-            .replace('<!-- hair -->', getLayer(`hair${hair}`))
-            .replace('<!-- eyes -->', getLayer(`eyes${eyes}`))
-            .replace('<!-- nose -->', getLayer(`nose${nose}`))
-            .replace('<!-- mouth -->', getLayer(`mouth${mouth}`))
-            .replace('<!-- beard -->', getLayer(`beard${beard}`, 0.5))
+  if (combination[takenNumbers]) {
+    createImage(idx);
+  } else {
+    const genName = getName(number, numberLength, max);
+    console.log(genName);
+    combination[takenNumbers] = combination;
 
-        const meta = {
-            name,
-            description: `A drawing of ${name.split('-').join(' ')}`,
-            image: `${idx}.png`,
-            attributes: [
-                { 
-                    beard: '',
-                    rarity: 0.5
-                }
-            ]
-        }
-        writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
-        writeFileSync(`./out/${idx}.svg`, final)
-        svgToPng(idx)
-    }
+    const final = template
+      .replace("<!-- width -->", widthOfImage * numberLength)
+      .replace("<!-- width -->", widthOfImage * numberLength)
+      .replace("<!-- bg1 -->", getLayer(`bg${bg}`))
+      .replace("<!-- alg1 -->", getLayer(String(number)[0]))
+      .replace("<!-- bg2 -->", numberLength >= 2 ? getLayer(`bg${bg}`) : "")
+      .replace(
+        "<!-- alg2 -->",
+        numberLength >= 2 ? getLayer(String(number)[1]) : ""
+      )
+      .replace("<!-- bg3 -->", numberLength >= 3 ? getLayer(`bg${bg}`) : "")
+      .replace(
+        "<!-- alg3 -->",
+        numberLength >= 3 ? getLayer(String(number)[2]) : ""
+      )
+      .replace("<!-- bg4 -->", numberLength >= 4 ? getLayer(`bg${bg}`) : "")
+      .replace(
+        "<!-- alg4 -->",
+        numberLength >= 4 ? getLayer(String(number)[3]) : ""
+      );
 
+    let name = `#${idx}`;
+    name = name.concat(`-${genName}`);
 
+    const meta = {
+      name,
+      description: `An image of the number ${genName.split("-").join(" ")}`,
+      image: `${idx}.png`,
+      attributes: [
+        {
+          digits: numberLength,
+          rarity: lengthProb.get(numberLength),
+        },
+      ],
+    };
+    writeFileSync(`./out/${idx}.json`, JSON.stringify(meta));
+    writeFileSync(`./out/${idx}.svg`, final);
+    svgToPng(idx);
+  }
 }
 
-
 // Create dir if not exists
-if (!existsSync('./out')){
-    mkdirSync('./out');
+if (!existsSync("./out")) {
+  mkdirSync("./out");
 }
 
 // Cleanup dir before each run
-readdirSync('./out').forEach(f => rmSync(`./out/${f}`));
+readdirSync("./out").forEach((f) => rmSync(`./out/${f}`));
 
+idx--;
 
 do {
-    createImage(idx);
-    idx--;
-  } while (idx >= 0);
+  createImage(idx);
+  idx--;
+} while (idx >= 0);
